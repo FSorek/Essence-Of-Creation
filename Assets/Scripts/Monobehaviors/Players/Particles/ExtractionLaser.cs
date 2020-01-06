69e2c73e2855f48c965578e4ed4cbcc8631b6cd4
fsorek <filip.sorek96@gmail.com>
2020-01-06 01:23:12 +0100

    Laser extraction beam

---
+++ Assets/Scripts/Monobehaviors/Players/Particles/ExtractionLaser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Data.Data_Types.Enums;
+using Data.ScriptableObjects.Player;
+using UnityEngine;
+
+public class ExtractionLaser : MonoBehaviour
+{
+    [SerializeField] private PlayerStateData playerStateData;
+    [SerializeField] private PlayerBuildData playerBuildData;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private Transform playerParticlePosition;
+
+    private void Awake()
+    {
+        if (lineRenderer.enabled)
+            lineRenderer.enabled = false;
+        
+        playerStateData.OnStateEntered += PlayerStateDataOnStateEntered;
+        playerStateData.OnStateExit += PlayerStateDataOnStateExit;
+    }
+
+    private void PlayerStateDataOnStateExit(PlayerStates state)
+    {
+        if(state != PlayerStates.EXTRACTING)
+            return;
+
+        lineRenderer.enabled = false;
+    }
+
+    private void PlayerStateDataOnStateEntered(PlayerStates state)
+    {
+        if(state != PlayerStates.EXTRACTING)
+            return;
+
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, playerParticlePosition.position);
+        lineRenderer.SetPosition(1, playerBuildData.TargetAttraction.transform.position);
+    }
+    
+    private void Update()
+    {
+        if(lineRenderer.enabled)
+            lineRenderer.SetPosition(0, playerParticlePosition.position);
+    }
+}
